37423c5518d97e14ac4f1b2405a362036895c58a
aleshenkova.E <aleshenkova.e@cdek.ru>
2025-03-17 23:28:22 +0300

    task2

--- src/figure.py
+++ src/figure.py
@@ -9,3 +9,9 @@ class Figure(ABC):
     @abstractmethod
     def get_perimeter(self):
         pass
+
+   @abstractmethod
+    def add_area(self):
+        if isinstance(figure, Figure):
+            return self.get_area() + figure.get_area()
+        else: raise ValueError("There is no figure")
